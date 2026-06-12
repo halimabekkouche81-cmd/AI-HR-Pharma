e6612e3c54ef871f2f3110f10350c4f3616ab840
Halima Bekkouche <halimabekkouche81@gmail.com>
2026-06-12 19:53:26 +0200

    Add deployment configuration

--- app/database.py
+++ app/database.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine(
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
     "postgresql+psycopg2://postgres:111111@127.0.0.1:5432/ai_hr_pharma_db"
 )
 
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(bind=engine)
+
 Base = declarative_base()
